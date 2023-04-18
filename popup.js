14fdcf07dec7e7d4d89684719ce8629ff85f8ef5
Hamza <hamzashakir149@gmail.com>
2023-04-18 13:15:48 +0530

    updated interaction on newsearch.html

--- popup.js
+++ popup.js
@@ -14,20 +14,20 @@
 */
 
 
-if (window.location.pathname == "/newsearch.html") {
+if (window.location.href.includes("newsearch.html")) {
+	console.log('this is newsearch.html');
+	const startScrapeBtn = document.querySelector(".start-search-footer");
+	const endScrapeBtn = document.querySelector(".end-search-footer");
+	const pauseScrapeBtn = document.querySelector(".pause-search");
+	const resumeScrapeBtn = document.querySelector(".resume-search");
 
-	const startScrapeBtn = document.querySelector("#start-search-footer");
-	const endScrapeBtn = document.querySelector("#end-search-footer");
-	const pauseScrapeBtn = document.querySelector("#pause-search");
-	const resumeScrapeBtn = document.querySelector("#resume-search");
-
-	document.querySelector("#start-scrape-btn").addEventListener("click", () => {
+	document.querySelector("#start-search-btn").addEventListener("click", () => {
 		startScrapeBtn.classList.add("hide");
 		endScrapeBtn.classList.remove("hide");
 		pauseScrapeBtn.classList.remove("hide");
 	})
 
-	document.querySelector("#end-scrape-btn").addEventListener("click", () => {
+	document.querySelector("#end-search-btn").addEventListener("click", () => {
 		startScrapeBtn.classList.remove("hide");
 		endScrapeBtn.classList.add("hide");
 		pauseScrapeBtn.classList.add("hide");
@@ -46,6 +46,6 @@ if (window.location.pathname == "/newsearch.html") {
 
 }
 
-/*else if (window.location.pathname == "/activity.html") {
-
+/*if (window.location.href.includes("home.html")) {
+	console.log('this is home.html');
 }*/
