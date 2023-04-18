f9e2b6a586e8694a34f09b9ec43ac9ce485493b4
Hamza <hamzashakir149@gmail.com>
2023-04-18 19:04:05 +0530

    op[timized code

--- content.js
+++ content.js
@@ -3,21 +3,6 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.requestType === "scrapeLeads") {
       console.log('received request from popup script to scrape data');
       
-      /*
-        let leads = document.querySelectorAll('.entity-result');
-  
-        let scrapedData = [...leads].map(lead => {
-            let leadImageElement = lead.querySelector('.presence-entity.presence-entity--size-3 img');
-            let leadImage = leadImageElement ? leadImageElement.getAttribute('src') : '';            
-            let leadName = lead.querySelector('.app-aware-link > span > span').innerText;
-            let leadTitle = lead.querySelector('.entity-result__primary-subtitle.t-14.t-black.t-normal').innerText;
-            let leadProfileLink = lead.querySelector('.app-aware-link').href;
-  
-            return { leadImage, leadName, leadTitle, leadProfileLink };
-        });
-        console.log(scrapedData);*/
-
-        
         let scrapedData = [];
 
         let leads = document.querySelectorAll('.entity-result');
@@ -27,12 +12,17 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             if (leads[i].querySelector('.artdeco-button__text').innerText == 'Connect') {
 
                 let leadName = leads[i].querySelector('.app-aware-link > span > span').innerText;
+                let leadFirstName = leadName.split(' ')[0];
+                let leadLastName = leadName.split(' ', 2)[1];
                 let leadTitle = leads[i].querySelector('.entity-result__primary-subtitle.t-14.t-black.t-normal').innerText;
                 let leadProfileLink = leads[i].querySelector('.app-aware-link').href;
-                let leadImage = leads[i].querySelector('.presence-entity.presence-entity--size-3 img').getAttribute('src');
+                let leadImageElement = leads[i].querySelector('.presence-entity.presence-entity--size-3 img');
+                let leadImage = leadImageElement ? leadImageElement.getAttribute('src') : ''; 
 
                 let leadData = {
-                  name: leadName,
+                  fullName: leadName,
+                  firstName: leadFirstName,
+                  lastName: leadLastName,
                   title: leadTitle,
                   profileLink: leadProfileLink,
                   image: leadImage
@@ -44,10 +34,6 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         }
         
 
-
-        /*if (request.requestType === "pauseScraping") {
-            
-        }*/
 
         // Send the scraped data to the background script and handle any errors
         chrome.runtime.sendMessage({ requestType: "storeData", data: scrapedData, keyName: "campaign 1" }, function(response) {
