1d4b9c2087b0f157c1460a994091ac64a3b0b82a
Hamza <hamzashakir149@gmail.com>
2023-04-20 01:31:56 +0530

    can now scrape multiple pages

--- content.js
+++ content.js
@@ -1,55 +1,184 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  
-    if (request.requestType === "scrapeLeads") {
-      console.log('received request from popup script to scrape data');
-      
-        let scrapedData = [];
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
-        let leads = document.querySelectorAll('.entity-result');
+  continueScraping = true;
 
-        for (let i = 0; i < leads.length; i++) {
+  // executes the following block when user sends a request to begin scraping
+  if (request.requestType === "scrapeLeads") {
+    console.log("recieved request from popup script to initiate scraping");
 
-            if (leads[i].querySelector('.artdeco-button__text').innerText == 'Connect') {
+    let scrapedData = [];
+    while (continueScraping) {
 
-                let leadName = leads[i].querySelector('.app-aware-link > span > span').innerText;
-                let leadFirstName = leadName.split(' ')[0];
-                let leadLastName = leadName.split(' ', 2)[1];
-                let leadTitle = leads[i].querySelector('.entity-result__primary-subtitle.t-14.t-black.t-normal').innerText;
-                let leadProfileLink = leads[i].querySelector('.app-aware-link').href;
-                let leadImageElement = leads[i].querySelector('.presence-entity.presence-entity--size-3 img');
-                let leadImage = leadImageElement ? leadImageElement.getAttribute('src') : ''; 
+      let leads = document.querySelectorAll('.entity-result');
 
-                let leadData = {
-                  fullName: leadName,
-                  firstName: leadFirstName,
-                  lastName: leadLastName,
-                  title: leadTitle,
-                  profileLink: leadProfileLink,
-                  image: leadImage
-                };
+      for (let i = 0; i < leads.length; i++) {
 
-                scrapedData.push(leadData);
+        if (leads[i].querySelector('.artdeco-button__text').innerText == 'Connect') {
 
-            }
+          let leadName = leads[i].querySelector('.app-aware-link > span > span').innerText;
+          let leadFirstName = leadName.split(' ')[0];
+          let leadLastName = leadName.split(' ', 2)[1];
+          let leadTitle = leads[i].querySelector('.entity-result__primary-subtitle.t-14.t-black.t-normal').innerText;
+          let leadProfileLink = leads[i].querySelector('.app-aware-link').href;
+          let leadImageElement = leads[i].querySelector('.presence-entity.presence-entity--size-3 img');
+          let leadImage = leadImageElement ? leadImageElement.getAttribute('src') : '';
+
+          let leadData = {
+            fullName: leadName,
+            firstName: leadFirstName,
+            lastName: leadLastName,
+            title: leadTitle,
+            profileLink: leadProfileLink,
+            image: leadImage
+          };
+
+          scrapedData.push(leadData);
         }
-        
+      }
 
+      console.log("scraped one page, now scrolling to the bottom of the screen");
 
-        // Send the scraped data to the background script and handle any errors
-        chrome.runtime.sendMessage({ requestType: "storeData", data: scrapedData, keyName: "campaign 1" }, function(response) {
-          console.log("Request sent to background script to store data");
+      
+      let nextPage = document.querySelector('.artdeco-pagination__button.artdeco-pagination__button--next.artdeco-button.artdeco-button--muted.artdeco-button--icon-right.artdeco-button--1.artdeco-button--tertiary.ember-view');
+      if (nextPage.disabled) {
+        //console.log("cannot click to go to next page");
+        continueScraping = false;
+        sendResponse({ message: "No more data to scrape" });
+      }
+      else {
+        setTimeout(nextPage.click(), 2000);
+      }
 
-          // receiving a response from the background script as a confirmation that the data has been stored in the local storage
-          console.log(response.message);
-
-          // sending response back to popup script confirming that the data has been scraped and stored in the local storage  
-          sendResponse({ message: "Data scraped and stored successfully" });        
-        });
-
-      return true;
+      chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        if (request.requestType === "pauseScraping") {
+          console.log("recieved request from popup script to pause scraping");
+          continueScraping = false;
+          sendResponse({ message: "scraping paused" });
+          return true;
+        }
+        if (request.requestType === "stopScraping") {
+          console.log("recieved request from popup script to stop scraping");
+          continueScraping = false;
+          sendResponse({ message: "scraping completed" });
+          return true;
+        }
+      });
     }
+  }
 
-    
-  });
+  //executes the following block if user resumes scraping after having paused it earlier
+  if (request.requestType === "resumeScraping") {
+    console.log("recieved request from popup script to resume scraping");
+
+    while (continueScraping) {
+
+      let leads = document.querySelectorAll('.entity-result');
+
+      for (let i = 0; i < leads.length; i++) {
+
+        if (leads[i].querySelector('.artdeco-button__text').innerText == 'Connect') {
+
+          let leadName = leads[i].querySelector('.app-aware-link > span > span').innerText;
+          let leadFirstName = leadName.split(' ')[0];
+          let leadLastName = leadName.split(' ', 2)[1];
+          let leadTitle = leads[i].querySelector('.entity-result__primary-subtitle.t-14.t-black.t-normal').innerText;
+          let leadProfileLink = leads[i].querySelector('.app-aware-link').href;
+          let leadImageElement = leads[i].querySelector('.presence-entity.presence-entity--size-3 img');
+          let leadImage = leadImageElement ? leadImageElement.getAttribute('src') : '';
+
+          let leadData = {
+            fullName: leadName,
+            firstName: leadFirstName,
+            lastName: leadLastName,
+            title: leadTitle,
+            profileLink: leadProfileLink,
+            image: leadImage
+          };
+
+          scrapedData.push(leadData);
+        }
+      }
+
+      window.scrollTo(0, document.body.scrollHeight);
+      let nextPage = document.querySelector('.artdeco-pagination__button.artdeco-pagination__button--next.artdeco-button.artdeco-button--muted.artdeco-button--icon-right.artdeco-button--1.artdeco-button--tertiary.ember-view');
+      if (nextPage.disabled) {
+        //console.log("cannot click to go to next page");
+        continueScraping = false;
+        sendResponse({ message: "No more data to scrape" });
+      }
+      else {
+        setTimeout(nextPage.click(), 2000);
+      }
+
+      chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        if (request.requestType === "pauseScraping") {
+          console.log("recieved request from popup script to pause scraping");
+          continueScraping = false;
+          sendResponse({ message: "scraping paused" });
+          return true;
+        }
+        if (request.requestType === "stopScraping") {
+          console.log("recieved request from popup script to stop scraping");
+          continueScraping = false;
+          sendResponse({ message: "scraping completed" });
+          return true;
+        }
+      });
+    }
+  }
   
+});
+/*
+  if (request.requestType === "scrapeLeads") {
+    console.log('received request from popup script to scrape data');
+    
+    let scrapedData = [];
+
+    let leads = document.querySelectorAll('.entity-result');
+
+    for (let i = 0; i < leads.length; i++) {
+
+      if (leads[i].querySelector('.artdeco-button__text').innerText == 'Connect') {
+
+        let leadName = leads[i].querySelector('.app-aware-link > span > span').innerText;
+        let leadFirstName = leadName.split(' ')[0];
+        let leadLastName = leadName.split(' ', 2)[1];
+        let leadTitle = leads[i].querySelector('.entity-result__primary-subtitle.t-14.t-black.t-normal').innerText;
+        let leadProfileLink = leads[i].querySelector('.app-aware-link').href;
+        let leadImageElement = leads[i].querySelector('.presence-entity.presence-entity--size-3 img');
+        let leadImage = leadImageElement ? leadImageElement.getAttribute('src') : ''; 
+
+        let leadData = {
+          fullName: leadName,
+          firstName: leadFirstName,
+          lastName: leadLastName,
+          title: leadTitle,
+          profileLink: leadProfileLink,
+          image: leadImage
+        };
+
+        scrapedData.push(leadData);
+
+        }
+    }
+    
+
+
+    // Send the scraped data to the background script and handle any errors
+    chrome.runtime.sendMessage({ requestType: "storeData", data: scrapedData, keyName: "campaign 1" }, function(response) {
+      console.log("Request sent to background script to store data");
+
+      // receiving a response from the background script as a confirmation that the data has been stored in the local storage
+      console.log(response.message);
+
+      // sending response back to popup script confirming that the data has been scraped and stored in the local storage  
+      sendResponse({ message: "Data scraped and stored successfully" });        
+    });
+
+  return true;
+}
+
+  
+});
+*/
 
--- popup.js
+++ popup.js
@@ -28,32 +28,63 @@ if (window.location.href.includes("newsearch.html")) {
 
 		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 			chrome.tabs.sendMessage(tabs[0].id, {"requestType": "scrapeLeads",  "url": tabs[0].url}, function(response) {
-			  console.log("Sent request to content script to scrape data");
+			  	console.log("Sent request to content script to scrape data");
 		
-			  //receiving a response from the content script confirming that the data has been scraped and stored in the local storage
-			  //console.log(response.message);
+			  	//receiving a response from the content script confirming that the data has been scraped and stored in the local storage
+			  	//console.log(response.message);
 			});
 			  
-		  });
+		});
 	})
 
 	document.querySelector("#end-search-btn").addEventListener("click", () => {
 		pauseScrapeBtn.classList.add("hide");
 		resumeScrapeBtn.classList.remove("hide");
+
+		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+			chrome.tabs.sendMessage(tabs[0].id, {"requestType": "stopScraping",  "url": tabs[0].url}, function(response) {
+				console.log("Sent request to content script to stop scraping");
+		
+			  	//receiving a response from the content script confirming that the data has been scraped and stored in the local storage
+			  	if (response.message === "scraping completed") {
+					window.location.href = "new-page.html";
+			  	}
+			});			  
+		});
 	})
 
 	document.querySelector("#pause-scrape-btn").addEventListener("click", () => {
 		pauseScrapeBtn.classList.add("hide");
 		resumeScrapeBtn.classList.remove("hide");
+
+		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+			chrome.tabs.sendMessage(tabs[0].id, {"requestType": "pauseScraping",  "url": tabs[0].url}, function(response) {
+			  	console.log("Sent request to content script to pause scraping");
+		
+			  	//receiving a response from the content script confirming that the data has been scraped and stored in the local storage
+			  	//console.log(response.message);
+			});
+ 
+		});
 	})
 
 	document.querySelector("#resume-scrape-btn").addEventListener("click", () => {
 		pauseScrapeBtn.classList.remove("hide");
 		resumeScrapeBtn.classList.add("hide");
+
+		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+			chrome.tabs.sendMessage(tabs[0].id, {"requestType": "resumeScraping",  "url": tabs[0].url}, function(response) {
+			  	console.log("Sent request to content script to resume scraping");
+		
+			  	//receiving a response from the content script confirming that the data has been scraped and stored in the local storage
+			  	//console.log(response.message);
+			});
+			  
+		});
 	})
 
 }
 
-/*if (window.location.href.includes("home.html")) {
+if (window.location.href.includes("message.html")) {
 	//console.log('this is home.html');
-}*/
+}
