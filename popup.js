e2b077c3856200fcf06fe00998de191751403358
Hamza <hamzashakir149@gmail.com>
2023-04-24 18:09:09 +0530

    updated name of a few classes & Ids

--- popup.js
+++ popup.js
@@ -1,18 +1,18 @@
 if (window.location.href.includes("newsearch.html")) {
 	console.log('this is newsearch.html');
 	const startScrapeBtn = document.querySelector(".start-search-footer");
-	const endScrapeBtn = document.querySelector(".end-search-footer");
+	const stopScrapeBtn = document.querySelector(".stop-search-footer");
 	const pauseScrapeBtn = document.querySelector(".pause-search");
 	const resumeScrapeBtn = document.querySelector(".resume-search");
-	//const port = chrome.tabs.connect(tabs[0].id, { name: "popup-to-content" });
 
-	document.querySelector("#start-search-btn").addEventListener("click", () => {
-		startScrapeBtn.classList.add("hide");
-		endScrapeBtn.classList.remove("hide");
-		pauseScrapeBtn.classList.remove("hide");
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		const port = chrome.tabs.connect(tabs[0].id, { name: "popup-to-content" });
 
-		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-			const port = chrome.tabs.connect(tabs[0].id, { name: "popup-to-content" });
+		document.querySelector("#start-search-btn").addEventListener("click", () => {
+			startScrapeBtn.classList.add("hide");
+			stopScrapeBtn.classList.remove("hide");
+			pauseScrapeBtn.classList.remove("hide");
+
 			port.postMessage({ action: "Start Scraping" });
 
 			port.onMessage.addListener(function(response) {
@@ -21,70 +21,47 @@ if (window.location.href.includes("newsearch.html")) {
 					// inject response.data onto popup
 				}
 			});
+		})
 
-			// chrome.tabs.sendMessage(tabs[0].id, {"requestType": "scrapeLeads",  "url": tabs[0].url}, function(response) {
-			//   	console.log("Sent request to content script to scrape data");
-				
-			  	//receiving a response from the content script confirming that the data has been scraped and can now be injected onto our popup script
-				//   if (chrome.runtime.lastError) {
-				// 	console.error(chrome.runtime.lastError);
-				//   } else {
-				// 	console.log(response.data);
-				//   }
+		document.querySelector("#stop-search-btn").addEventListener("click", () => {
+			pauseScrapeBtn.classList.add("hide");
+			resumeScrapeBtn.classList.remove("hide");
+	
+			port.postMessage({ action: "Stop Scraping" });
 
-				//injecting scraped content onto popup script one by one for every page that is scraped
-				
-			// });
-			  
-		});
-	})
-
-	document.querySelector("#end-search-btn").addEventListener("click", () => {
-		pauseScrapeBtn.classList.add("hide");
-		resumeScrapeBtn.classList.remove("hide");
-
-		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, {"requestType": "stopScraping",  "url": tabs[0].url}, function(response) {
-				console.log("Sent request to content script to stop scraping");
-		
-			  	//receiving a response from the content script confirming that the data has been scraped and stored in the local storage
-			  	if (response.message === "scraping completed") {
-					window.location.href = "message.html";
-			  	}
-			});			  
-		});
-	})
-
-	document.querySelector("#pause-scrape-btn").addEventListener("click", () => {
-		pauseScrapeBtn.classList.add("hide");
-		resumeScrapeBtn.classList.remove("hide");
-
-		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, {"requestType": "pauseScraping",  "url": tabs[0].url}, function(response) {
-			  	console.log("Sent request to content script to pause scraping");
-		
-			  	//receiving a response from the content script confirming that the data has been scraped and stored in the local storage
-			  	//console.log(response.message);
+			port.onMessage.addListener(function(response) {
+				if (response.message === "Stopped Scraping" ) {
+					
+				}
 			});
- 
-		});
-	})
+		})
 
-	document.querySelector("#resume-scrape-btn").addEventListener("click", () => {
-		pauseScrapeBtn.classList.remove("hide");
-		resumeScrapeBtn.classList.add("hide");
+		document.querySelector("#pause-scrape-btn").addEventListener("click", () => {
+			pauseScrapeBtn.classList.add("hide");
+			resumeScrapeBtn.classList.remove("hide");
+			
+			port.postMessage({ action: "Pause Scraping" });
 
-		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, {"requestType": "resumeScraping",  "url": tabs[0].url}, function(response) {
-			  	console.log("Sent request to content script to resume scraping");
-		
-			  	//receiving a response from the content script confirming that the data has been scraped and stored in the local storage
-			  	//console.log(response.message);
+			port.onMessage.addListener(function(response) {
+				if (response.message === "Paused Scraping" ) {
+					
+				}
 			});
-			  
-		});
-	})
+		})
+	
+		document.querySelector("#resume-scrape-btn").addEventListener("click", () => {
+			pauseScrapeBtn.classList.remove("hide");
+			resumeScrapeBtn.classList.add("hide");
+			
+			port.postMessage({ action: "Resume Scraping" });
 
+			port.onMessage.addListener(function(response) {
+				if (response.message === "Resumed Scraping" ) {
+					
+				}
+			});
+		})
+	});
 }
 
 if (window.location.href.includes("message.html")) {
